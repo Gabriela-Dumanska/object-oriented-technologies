892b7ab1de15dbc4235aad3c562806c1d3ea0179
Gabriela Dumańska <dumanska@student.agh.edu.pl>
2024-10-31 16:54:35 +0100

    Lab 3 completed

--- lab-rx-lab 3/src/main/java/app/CrawlerApp.java
+++ lab-rx-lab 3/src/main/java/app/CrawlerApp.java
@@ -8,14 +8,16 @@ public class CrawlerApp {
 
     public static final String SCRAPER_API_KEY = "11e4f3cda35b231d9e4058a970625f85";
 
-    private static final List<String> TOPICS = List.of("Agent Cooper", "Sherlock", "Poirot", "Detective Monk");
+    private static final List<String> TOPICS = List.of("cat", "rat", "dog", "hamster");
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         PhotoCrawler photoCrawler = new PhotoCrawler();
         photoCrawler.resetLibrary();
         //photoCrawler.downloadPhotoExamples();
-        photoCrawler.downloadPhotosForQuery("rats");
-//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+        //photoCrawler.downloadPhotosForQuery("rats");
+        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+
+        Thread.sleep(100_000);
     }
 }
--- lab-rx-lab 3/src/main/java/app/PhotoCrawler.java
+++ lab-rx-lab 3/src/main/java/app/PhotoCrawler.java
@@ -1,12 +1,14 @@
 package app;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import model.Photo;
 import util.PhotoDownloader;
 import util.PhotoProcessor;
 import util.PhotoSerializer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,13 +48,27 @@ public class PhotoCrawler {
             photoDownloader.searchForPhotos(query)
                     .subscribe(photoSerializer::savePhoto,
                             error -> log.log(Level.SEVERE, "Problems with DuckDuckGo :("),
-                            onCompleted -> log.log(Level.INFO, "Downloading completed :)");
+                            () -> log.log(Level.INFO, "Downloading completed :)"));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void downloadPhotosForMultipleQueries(List<String> queries) {
-        // TODO Implement me :(
+    public void downloadPhotosForMultipleQueries(List<String> queries) throws IOException {
+        List<Observable<Photo>> observables = new ArrayList<>();
+        try{
+            for(String query : queries) {
+                observables.add(photoDownloader.searchForPhotos(query)
+                        .subscribeOn(Schedulers.io()));
+            }
+            Observable.merge(observables)
+                    .subscribe(photoSerializer::savePhoto,
+                            error -> log.log(Level.SEVERE, "Problems with DuckDuckGo :("),
+                            () -> log.log(Level.INFO, "Downloading completed :)"));
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
